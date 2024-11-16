686516fb233f760f38bf9a980bcf99c94d58b625
Ashraful Islam <ashrafulislam.dev1@gmail.com>
2024-11-16 23:06:06 +0600

    Completed banner section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,9 +3,11 @@ import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div className="min-h-20 bg-red-500 flex justify-between items-center px-8">
-      <div>Teeth Wizard</div>
-      <div className="space-x-4 text-white">
+    <div className="min-h-20 bg-blue-950 text-white flex justify-between items-center px-8">
+      <div>
+        <h1 className="text-xl font-bold">Teeth Wizard</h1>
+      </div>
+      <div className="space-x-4">
         <NavLink to="/">Home</NavLink>
         <NavLink to="/all-treatment">All Treatment</NavLink>
         <NavLink to="/my-appointment">My Appointment</NavLink>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
+import Banner from "../components/Banner";
 import Navbar from "../components/Navbar";
 
 const Home = () => {
-  return <div className="text-red-500 text-lg ">Home</div>;
+  return (
+    <div>
+      <Banner />
+    </div>
+  );
 };
 
 export default Home;
